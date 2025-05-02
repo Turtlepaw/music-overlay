7376a1bf5f8b6b7500e63f690b6d28541f621396
Turtlepaw <81275769+Turtlepaw@users.noreply.github.com>
2025-05-02 00:52:26 -0400

    feat: setup fastlane

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,16 @@
 import com.android.build.api.dsl.Packaging
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("com.google.devtools.ksp") version "2.0.21-1.0.27"
+}
+
+val keystoreProperties = Properties().apply {
+    val file = rootProject.file("key.properties")
+    if (file.exists()) load(file.inputStream())
 }
 
 android {
@@ -15,10 +21,18 @@ android {
         applicationId = "com.turtlepaw.overlay"
         minSdk = 30
         targetSdk = 35
-        versionCode = 9
+        versionCode = 10
         versionName = "1.0"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -26,6 +40,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
